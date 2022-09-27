3a616ea3739af7733366d79f6b0b964dc22c9be0
Wouter Bosch <wfcbosch@gmail.com>
2022-09-27 15:58:16 +0100

    update so that it works every year

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 window.onload = function(){
-
-var countDownDate = new Date("Dec 25, 2019 00:00:00").getTime();
-
-
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  if (distance < 0) {
-    clearInterval(x);
-
-    if(new Date().getMonth == 12 && (new Date().getDay == 25 || new Date().getDay) == 26){
-        document.getElementById("countdown").innerHTML = "Merry Christmas!!!";
-    }
-    else{
-        document.getElementById("countdown").innerHTML = "EXPIRED";
-    }
+    const year = new Date().getFullYear()
+    const countDownDate = new Date("Dec 25, " + year + " 00:00:00").getTime();
 
 
-  }
-}, 1000);
+    const x = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        
+        if (distance < 0) {
+            clearInterval(x);
+
+            if(new Date().getMonth == 12 && (new Date().getDay == 25 || new Date().getDay) == 26){
+                document.getElementById("countdown").innerHTML = "Merry Christmas!!!";
+            }
+            else{
+                document.getElementById("countdown").innerHTML = "EXPIRED";
+            }
+
+
+        }
+    }, 1000);
 }
